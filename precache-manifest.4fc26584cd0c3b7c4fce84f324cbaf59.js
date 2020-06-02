4e50837344a930d04cb899b9f4aac4f04fdedda3
w3tsa <tanveersayem@gmail.com>
2020-06-01 19:03:20 -0700

    Updates

---
+++ precache-manifest.4fc26584cd0c3b7c4fce84f324cbaf59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a6cae8638a2ace0660279a2549b16b",
+    "url": "/robot-friends/index.html"
+  },
+  {
+    "revision": "ea5f49909766c44fc994",
+    "url": "/robot-friends/static/css/main.e9eb610b.chunk.css"
+  },
+  {
+    "revision": "83a20a0377fe7c7f0680",
+    "url": "/robot-friends/static/js/2.1856ea97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot-friends/static/js/2.1856ea97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea5f49909766c44fc994",
+    "url": "/robot-friends/static/js/main.c494bbcf.chunk.js"
+  },
+  {
+    "revision": "2d9cfb175417421d185a",
+    "url": "/robot-friends/static/js/runtime-main.8557caaf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
